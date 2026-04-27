71ea2755d5cb0a728d7b53ec31db994ef4316c5a
Mikle Seregin <mikhail-seregin@yandex.ru>
2026-04-27 20:05:18 +0300

    Update

--- lesson2/2-abstarct/index.php
+++ lesson2/2-abstarct/index.php
@@ -2,52 +2,60 @@
 
 namespace Sample2;
 
-abstract class Tag{
-	protected array $attrs = [];
+abstract class Tag
+{
+    protected array $attrs = [];
 
-	public function __construct(protected string $name)
-	{
-		
-	}
+    public function __construct(protected string $name)
+    {
 
-	abstract public function render();
+    }
 
-	public function attr(string $name, string $value){ // : static
-		$this->attrs[$name] = $value;
-		return $this;
-	}
+    abstract public function render();
 
-	protected function attrsToStr() : string{
-		$str = '';
+    public function attr(string $name, string $value)
+    { // : static
+        $this->attrs[$name] = $value;
+        return $this;
+    }
 
-		foreach($this->attrs as $name => $value){
-			$str .= " $name=\"$value\"";
-		}
+    protected function attrsToStr(): string
+    {
+        $str = '';
 
-		return trim($str);
-	}
+        foreach ($this->attrs as $name => $value) {
+            $str .= " $name=\"$value\"";
+        }
+
+        return trim($str);
+    }
 }
 
-class SingleTag extends Tag{
-	public function render() : string{
-		$attrs = $this->attrsToStr();
-		return "<$this->name $attrs>";
-	}
+class SingleTag extends Tag
+{
+    public function render(): string
+    {
+        $attrs = $this->attrsToStr();
+        return "<$this->name $attrs>";
+    }
 }
 
-class PairTag extends Tag{
-	protected array $children = [];
+class PairTag extends Tag
+{
+    protected array $children = [];
 
-	public function appendChild(Tag $child){
-		$this->children[] = $child;
-		return $this;
-	}
+    public function appendChild(Tag $child)
+    {
+        $this->children[] = $child;
+        return $this;
+    }
 
-	public function render() : string{
-		$attrs = $this->attrsToStr();
-		$childrenStr = implode('', array_map(fn(Tag $child) => $child->render(), $this->children));
-		return "<$this->name $attrs>$childrenStr</$this->name>";
-	}
+    public function render(): string
+    {
+        $attrs = $this->attrsToStr();
+        $childrenStr = implode('', array_map(fn(Tag $child) => $child->render(), $this->children));
+        return "<$this->name $attrs>$childrenStr</$this->name>";
+    }
 }
 
 /* class MultiTag extends Tag{
